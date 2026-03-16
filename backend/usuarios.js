3490ddc5d7022b3761e0898226c794d4392be925
Yasmin <yasminkarolayne64@gmail.com>
2026-03-16 09:41:58 -0300

    Update usuarios.js

--- backend/usuarios.js
+++ backend/usuarios.js
@@ -9,53 +9,40 @@ const authUsers = (pool, jwt, bcrypt, jwtSecret) => {//funcao para autenticar us
    
     
     try {
-        const { email, senha } = request.body//pega o email e a senha do campo de requisicao
+       const result = await pool.query(//verificar se o email existe no banco de dados   
+            "SELECT  "id", "password" FROM usuarios WHERE email=$1",//  consulta sql para selecionar o usuario com o email fornecido                                            
+             [username]//passa o email como parametro para evitar sql injection
+     );
+    } cacth (error){
+        console.log (error);
+    } const senha= result.rows[0].password;
+            await bcrypt.compare(password, senha).then(() => {
+             //Gerando um token de 1hora de duração para segurança.
+                const token = jwt.sign({username, id: result.rows[0].id}, jwtSecret, {expiresIn: '1h'});
+                const tokenIssuedAt = jwt.verify(token, jwtSecret).iat;
+                const tokenDuration = jwt.verify(token, jwtSecret).exp;
 
-        const user = await pool.query(//verificar se o email existe no banco de dados   
-            "SELECT * FROM usuarios WHERE email=$1",//  consulta sql para selecionar o usuario com o email fornecido                                            
-             [email]//passa o email como parametro para evitar sql injection
-     )
 
-    if(user.rows.length === 0){//se o email nao existir no banco de dados, ele cria outro usuario
-
-        const hash = await bcrypt.hash(senha,10)//gera um hash da senha para armazanar no banco de dados
-
-    //insere o email e asenha hash no banco de dados para criar o usuario 
-    await pool.query(`
-    "INSERT INTO usuarios (email,senha) 
-         VALUES ($1,$2)",
-            [email,hash]
-    `)
-
-    return reply.send({
-     mensagem:"Usuário criado"
-    })
-    }
-        const usuario = user.rows[0]//pega o usuario do banco de dados
-
-     const senhaValida = await bcrypt.compare(senha,usuario.senha)//compara a senha digitada com a senha hash armazenada no banco 
-
-    if(!senhaValida){
-        return reply.status(401).send({
-            erro:"Senha inválida"
-        })
-    }
-
-    const token = jwt.sign(//gera um token jwt com o id e email do usuario, a chave secreta e o tempo de epiraçao de 1h
-         { id: usuario.id, email: usuario.email },jwtSecret,
-         { expiresIn: "1h" }
-    )
-
-    return reply.send({
-        token
-})
+            //Devolvendo o token para o usuário utilizar nas Endpoints.
+            //Aqui eu deixei o mais detalhado possivel para o usuário ter dados de quando termina seu acesso a API.
+                return rep.code(200).send({
+                    username,
+                    token,
+                    tokenIssuedAt,
+                    tokenDuration,
+                    tokenIssuedTimestamp: new Date(tokenIssuedAt * 1000).toLocaleString("pt-BR"),
+                    tokenDurationTimestamp: new Date(tokenDuration * 1000).toLocaleString("pt-BR")
+                });
+   
+            });
 
    }catch(error){
         return reply.status(500).send({
         erro:error.message
     })
-
-    }
+    finally{
+            connection.release();
+        }
 
     }
 
